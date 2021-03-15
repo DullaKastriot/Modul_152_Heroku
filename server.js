00780b6ab93dfc4a64de00329ee66e6fb7476609
Kastriot Dulla <62029824+CsBeKastriot@users.noreply.github.com>
2021-03-15 23:18:20 +0100

    Added the third endpoint

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+const MulterSharpResizer = require("multer-sharp-resizer");
 const express = require('express');
 const sass = require('node-sass');
+var multer = require("multer");
+var sharp = require("sharp");
 const less = require('less');
 const fs = require('fs');
 const app = express();
@@ -42,6 +45,104 @@ app.post('/api/css/less', function (request, restore) {
         }
     });
 });
+
+// Binding the instance of the app
+app.use(express.static(`${__dirname}/upload`));
+app.use(express.json());
+
+// Store the picture objects as storage
+const multerStorage = multer.memoryStorage();
+
+// Filter files
+const multerFilter = (req, file, callback) => {
+    if (file.mimeType.startsWith('image')) {
+        callback(null, true);
+    }
+    else {
+        callback(null, false);
+    }
+}
+
+const upload = multer({
+    storage: multerStorage,
+    fileFilter: multerFilter
+});
+
+// Here we crate the upload fields. Uncomment the second field in case you want to upload multiple images.
+const uploadImages = upload.fields([
+    // {name: "pictures ", maxCount: 5},
+    {name: "file", maxCount: 1}
+]);
+
+// multer .array() and .single(). For when you want to upload only one image or many pictures.
+// const uploadImages = upload.array('pictures', 5);
+// const uploadImages = upload.single('file');
+
+// Create resize function and give it the values it needs.
+const resizeImages = async(req, res, next) => {
+    // Create date to be used in creation. Might come in handy to know when I uploaded what thing.
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = `${today.getMonth + 1}`.padStart(2, "0");
+
+    const filename = {
+        file: `${Date.now()}`
+    }
+
+    const sizes = [{
+        path: "original",
+        width: null,
+        height: null
+    },
+        {
+            path: "small__",
+            width: 720
+        },
+        {
+            path: "medium__",
+            width: 1280
+        },
+        {
+            path: "large__",
+            width: 1920
+        },
+        {
+            path: "thumbnail__",
+            width: 360
+        }
+    ];
+
+    // Create upload path for static files
+    const uploadPath = `./upload/${year}/${month}`;
+    const fileUrl = `${req.protocol}://${req.get("host")}/upload/${year}/${month}`;
+
+    // Sharp options.
+    const sharpOptions = {
+        fit: "contain",
+        background: {r:255, g:255, b:255}
+    }
+
+    // Resize objects.
+    const resizeObjects = new MulterSharpResizer(req, filename, sizes, uploadPath, fileUrl, sharpOptions);
+
+    // Call resize method to resize files
+    await resizeObjects.resize();
+    const getDataUploaded = resizeObjects.getData();
+
+    // Get details of uploaded files. Is used by multer
+    req.body.file = getDataUploaded.file;
+    next();
+}
+
+const createPicture = async(req, res, next) => {
+    res.status(200) .json({
+        status: "succsess",
+        file: req.body.file
+    })
+};
+
+// Third endpoint.
+app.post("/api/file", uploadImages, resizeImages, createPicture);
 
 // Added port variable in case to listen to port 3000
 app.listen(process.env.PORT || port);
